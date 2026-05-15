e05f56fa6f598d0554596696801161049161cf68
sadhanakannan18-lgtm <sadhanakannan18@gmail.com>
2026-05-15 16:58:19 +0530

    Update FileHandlingUtility.java

--- Task1 - FileHandlingUtility/FileHandlingUtility.java
+++ Task1 - FileHandlingUtility/FileHandlingUtility.java
@@ -1,2 +1,31 @@
 Task 1 files
+import java.io.*;
 
+public class FileHandlingUtility {
+    public static void main(String[] args) {
+        try {
+
+            FileWriter writer = new FileWriter("sample.txt");
+            writer.write("Hello, this is CODTECH Internship Task 1.\n");
+            writer.write("Learning File Handling in Java.");
+            writer.close();
+
+            System.out.println("Data written successfully.\n");
+
+            BufferedReader reader = new BufferedReader(new FileReader("sample.txt"));
+
+            String line;
+
+            System.out.println("Reading File:\n");
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
