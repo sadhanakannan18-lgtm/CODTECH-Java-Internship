5b8159453225107171388f2c59c74b73b853ec0d
sadhanakannan18-lgtm <sadhanakannan18@gmail.com>
2026-05-15 17:15:15 +0530

    Update RestAPIClient.java

--- Task2 - RestAPIClient/RestAPIClient.java
+++ Task2 - RestAPIClient/RestAPIClient.java
@@ -1,1 +1,46 @@
 Task 2 files
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RestAPIClient {
+
+    public static void main(String[] args) {
+
+        try {
+
+            String apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
+
+            URL url = new URL(apiUrl);
+
+            HttpURLConnection connection =
+                    (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            System.out.println("Response Code: " + responseCode);
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = reader.readLine()) != null) {
+                response.append(inputLine);
+            }
+
+            reader.close();
+
+            System.out.println("\nAPI Response:");
+            System.out.println(response.toString());
+
+        } catch (Exception e) {
+
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
